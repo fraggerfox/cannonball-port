6d1c05f352c2e03daf510d35c781bb324ec4190e
fraggerfox <santhosh.raju@gmail.com>
2019-08-18 17:11:37 -0500

    Streamlined the patches to be more generic.
    Updated the patches to be generic enough to include the BSDs.
    Upstream of patches have been requested with the maintainer.
    
    Adds a man page on how to configure cannonball.

--- emulators/cannonball/files/patch-cmake_bsd.cmake
+++ emulators/cannonball/files/patch-cmake_bsd.cmake
@@ -1,11 +1,14 @@
-Create a cmake file for FreeBSD build.
+Create a cmake file for BSD based builds.
 
---- cmake/bsd.cmake.orig	2019-06-10 03:23:23 UTC
+--- cmake/bsd.cmake.orig        2019-08-18 00:41:57.311525123 +0000
 +++ cmake/bsd.cmake
-@@ -0,0 +1,20 @@
-+# Default CMake Setup. Used for FreeBSD Builds.
+@@ -0,0 +1,23 @@
++# Default CMake Setup. Used for BSD Builds.
 +
-+set(lib_base /usr/local/include)
++set(lib_base ${BSD_PREFIX_PATH}/include)
++
++set(BOOST_INCLUDEDIR ${lib_base})
++
 +set(sdl_root ${lib_base}/SDL)
 +
 +include_directories("${sdl_root}")
@@ -16,10 +19,11 @@ Create a cmake file for FreeBSD build.
 +
 +# Linking
 +link_directories(
-+    "/usr/local/lib"
++    "${BSD_PREFIX_PATH}/lib"
 +)
 +
 +# Location for Cannonball to create save files
 +# Used to auto-generate setup.hpp with various file paths
 +set(xml_directory ./)
 +set(sdl_flags "SDL_DOUBLEBUF | SDL_SWSURFACE")
+
